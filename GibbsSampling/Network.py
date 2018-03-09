ffe96ccb4c1d234439aa62418da76ae818be6497
Augusto Wong <augustowong2010@hotmail.com>
2018-03-09 01:46:38 -0500

    added all but last table

--- GibbsSampling/Network.py
+++ GibbsSampling/Network.py
@@ -51,6 +51,78 @@ class Network:
 
         schools.children.append(price)
 
+        #Add tables
+        amenitiespTable = [0 for x in range(2)]
+        #lots=0 little=1
+        amenitiespTable[0]=0.3
+        amenitiespTable[1]= 1 - amenitiespTable[0]
+        amenities.pTable = amenitiespTable
+
+        nbpTable = [0 for x in range(2)]
+        #bad=0 good=1
+        nbpTable[0]=0.4
+        nbpTable[1]= 1 - nbpTable[0]
+        neighborhood.pTable = nbpTable
+
+        sizepTable = [0 for x in range(3)]
+        # small=0 medium=1 large=2
+        sizepTable[0]=0.33
+        sizepTable[1]=0.34
+        sizepTable[2]=0.33
+        size.pTable = sizepTable
+
+        childrenpTable = [[0 for x in range(2)] for y in range(2)]
+        #Bad=0 good=1
+        childrenpTable[0][0]=0.6
+        childrenpTable[0][1]=0.4
+        childrenpTable[1][0]=0.3
+        childrenpTable[1][1]=0.7
+        children.pTable = childrenpTable
+
+        schoolspTable = [[0 for x in range(2)] for y in range(2)]
+        #Bad=0 good=1
+        schoolspTable[0][0]=0.7
+        schoolspTable[0][1]=0.3
+        schoolspTable[1][0]=0.8
+        schoolspTable[1][1]=0.2
+        schools.pTable = schoolspTable
+
+        agepTable = [[0 for x in range(3)] for y in range(3)]
+        #Old, 0=bad 1=good 2=ugly
+        agepTable[0][0]=0.6
+        agepTable[1][0]=0.3
+        agepTable[2][0]=0.9
+        #New
+        agepTable[0][1]=0.4
+        agepTable[1][1]=0.7
+        agepTable[2][1]=0.1
+        age.pTable = agepTable
+
+
+        locationpTable = [[[0 for k in xrange(3)] for j in xrange(3)] for i in xrange(3)]
+        #lots=0 bad=0
+        #little=1 good=1
+
+        #Bad=0
+        locationpTable[0][0][0]=0.4
+        locationpTable[0][1][0]=0.15
+        locationpTable[1][0][0]=0.4
+        locationpTable[1][1][0]=0.35
+
+        #Good=1
+        locationpTable[0][0][1]=0.3
+        locationpTable[0][1][1]=0.8
+        locationpTable[1][0][1]=0.2
+        locationpTable[1][1][1]=0.5
+
+        #Ugly=2
+        locationpTable[0][0][2]=0.3
+        locationpTable[0][1][2]=0.05
+        locationpTable[1][0][2]=0.4
+        locationpTable[1][1][2]=0.15
+
+        #Missing last table [][][][][]
+
         #Add created nodes into self.nodes list
         self.nodes.append(amenities)
         self.nodes.append(neighborhood)
